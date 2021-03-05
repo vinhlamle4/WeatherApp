59d9987da57699b8b108c38ecf32438072c29d8e
vinhlamle4 <vinhlamle4@gmail.com>
2021-03-05 14:33:08 +0700

    update
    update gradle, only support portrait orientation

--- app/src/main/java/com/example/weatherapp/ui/splash_screen/SplashActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/splash_screen/SplashActivity.kt
@@ -1,8 +1,9 @@
 package com.example.weatherapp.ui.splash_screen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.CountDownTimer
 import com.example.weatherapp.ui.main.view.MainActivity
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
@@ -12,9 +13,15 @@ import kotlinx.coroutines.FlowPreview
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        object: CountDownTimer(500, 500) {
+            override fun onTick(p0: Long) {
+            }
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+            override fun onFinish() {
+                val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }.start()
     }
 }
